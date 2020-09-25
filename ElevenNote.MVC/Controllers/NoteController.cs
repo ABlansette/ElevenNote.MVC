9cc5baf28e3e9513cc53d39d6bd27951313a69d2
Alexander.Blansette@gmail.com <Alexander.Blansette@gmail.com>
2020-09-25 14:04:38 -0400

    More!

--- ElevenNote.MVC/Controllers/NoteController.cs
+++ ElevenNote.MVC/Controllers/NoteController.cs
@@ -1,4 +1,6 @@
-﻿using ElevenNote.Models;
+﻿using EelevenNote.Services;
+using ElevenNote.Models;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +15,10 @@ namespace ElevenNote.MVC.Controllers
         [Authorize]
         public ActionResult Index()
         {
-            var model = new NoteListItem[0];
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            var model = service.GetNotes();
+
             return View(model);
         }
 
@@ -27,11 +32,26 @@ namespace ElevenNote.MVC.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(NoteCreate model)
         {
-            if (ModelState.IsValid)
-            {
+            if (!ModelState.IsValid) return View(model);
 
-            }
+            var service = CreateNoteService();
+
+            if (service.CreateNote(model))
+            {
+                ViewBag.SaveResult = "Your note was created.";
+                return RedirectToAction("Index");
+            };
+
+            ModelState.AddModelError("", "Note could not be created.");
+
             return View(model);
+        }
+
+        private NoteService NewMethod()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            return service;
         }
     }
 }
