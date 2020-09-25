76aec88564c439f1820be86d161e02b7f5adcbc6
Alexander.Blansette@gmail.com <Alexander.Blansette@gmail.com>
2020-09-25 10:31:14 -0400

    NoteController

---
+++ ElevenNote.Data/Migrations/Configuration.cs
@@ -0,0 +1,25 @@
+﻿    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+namespace ElevenNote.MVC.Migrations
+{
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ElevenNote.MVC.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "ElevenNote.MVC.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(ElevenNote.MVC.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
